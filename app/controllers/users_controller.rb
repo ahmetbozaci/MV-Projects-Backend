e63a1f7f9f0efeb6f5f171cdd9519c0fc70701a4
Ahmet Bozaci <ahmt9417@gmail.com>
2022-04-06 01:40:44 +0300

    Create user model, clean files for api

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: {
+        status: :created,
+        user: @user
+      }
+    else
+      render json: {
+        status: 500,
+        errors: @user.errors.full_messages
+      }
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name)
+    end
+end
