d17d1486213eb03fb67053fffa9f2fa8a3ad968e
Ahmet Bozaci <ahmt9417@gmail.com>
2022-04-06 06:00:22 +0300

    Create project model

---
+++ db/migrate/20220406025945_create_projects.rb
@@ -0,0 +1,13 @@
+class CreateProjects < ActiveRecord::Migration[7.0]
+  def change
+    create_table :projects do |t|
+      t.string :repo_link
+      t.string :live_link
+      t.string :picture
+      t.string :description
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
